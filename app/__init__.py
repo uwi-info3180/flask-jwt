f2edea8fe994fbd23cc803382693ee8cad88474d
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2020-05-16 16:18:30 -0500

    Remove Flask-JWT and stick with PyJWT

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from flask_jwt import JWT
 from app.models import authenticate, identity
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret'
-
-# jwt = JWT(app, authenticate, identity)
 
 from app import views
