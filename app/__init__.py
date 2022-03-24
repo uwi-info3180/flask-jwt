e092b8f70167a610d52b7744496a3f1199bdd4df
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-03-24 00:09:27 -0500

    Add config and use python-dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from app.models import authenticate, identity
+from .config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super-secret'
+app.config.from_object(Config)
 
 from app import views
