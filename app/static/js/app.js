8632275a2ee293a467761af8d15f44faa47ff87c
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2024-03-28 09:33:10 -0500

    Update to Vue3 with composition api

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,19 +1,16 @@
 /* Add your Application JavaScript */
+const { createApp, ref } = Vue
+
 const app = Vue.createApp({
-  data() {
-    return {
-      result: "The result will appear here.",
-      token: "",
-      tasks: [],
-      error: false,
-    };
-  },
-  methods: {
+  setup() {
+    const result = ref("The result will appear here.");
+    const token = ref("");
+    const tasks = ref([]);
+    const error = ref(false);
+
     // Usually the generation of a JWT will be done when a user either registers
     // with your web application or when they login.
-    getToken() {
-      let self = this;
-
+    function getToken() {
       fetch("/token")
         .then(function (response) {
           return response.json();
@@ -25,18 +22,20 @@ const app = Vue.createApp({
           // can use the token until it expires or is deleted.
           localStorage.setItem("token", jwt_token);
           console.info("Token generated and added to localStorage.");
-          self.token = jwt_token;
+          token.value = jwt_token;
         });
-    },
+    }
+
     // Remove token stored in localStorage.
     // Usually you will remove it when a user logs out of your web application
     // or if the token has expired.
-    removeToken() {
+    function removeToken() {
       localStorage.removeItem("token");
       console.info("Token removed from localStorage.");
       alert("Token removed!");
-    },
-    getSecure() {
+    }
+
+    function getSecure() {
       let self = this;
       fetch("/api/secure", {
         headers: {
@@ -56,16 +55,16 @@ const app = Vue.createApp({
           alert.classList.add("alert-success");
 
           if (response.data) {
-            let result = response.data;
+            let resultData = response.data;
             // successful response
-            self.result = `Congrats! You have now made a successful request with a JSON Web Token. Name is: ${result.user.name}.`;
+            result.value = `Congrats! You have now made a successful request with a JSON Web Token. Name is: ${resultData.user.name}.`;
           } else {
             let alert = document.querySelector(".alert");
             alert.classList.remove("alert-info");
             alert.classList.add("alert-danger");
 
             // unsuccessful response (ie. there was an error)
-            self.result = `There was an error. ${response.description}`;
+            result.value = `There was an error. ${response.description}`;
           }
         })
         .catch(function (error) {
@@ -73,26 +72,28 @@ const app = Vue.createApp({
           alert.classList.remove("alert-info");
           alert.classList.add("alert-danger");
           // unsuccessful response (ie. there was an error)
-          self.result = `There was an error.`;
+          result.value = `There was an error.`;
         });
-    },
+    }
     // Visit the unsecure route which doesn't need a JWT token or
     // Authorization header
-    getUnsecure() {
-      let self = this;
+    function getUnsecure() {
+      // let self = this;
       fetch("/api/unsecure")
         .then(function (response) {
           return response.json();
         })
         .then(function (response) {
-          let result = response.data;
+          let resultData = response.data;
           let alert = document.querySelector(".alert");
           alert.classList.remove("alert-danger");
           alert.classList.add("alert-info");
-          self.result = `You visited the unsecure route that didn't require a JSON Web Token. Name is: ${result.user.name}.`;
+          result.value = `You visited the unsecure route that didn't require a JSON Web Token. Name is: ${resultData.user.name}.`;
+          console.log(result.value);
         });
-    },
-    getTasks() {
+    }
+
+    function getTasks() {
       let self = this;
       fetch(
         "/api/tasks",
@@ -112,18 +113,30 @@ const app = Vue.createApp({
         .then(function (data) {
           if (data.tasks) {
             console.log(data.tasks);
-            self.tasks = data.tasks;
-            self.error = false;
+            tasks.value = data.tasks;
+            error.value = false;
           } else {
-            self.error = `Error getting tasks. ${data.description}`;
-            self.tasks = [];
+            error.value = `Error getting tasks. ${data.description}`;
+            tasks.value = [];
           }
         })
         .catch(function (err) {
           console.log(err);
         });
-    },
-  },
+    }
+
+    return {
+      result,
+      token,
+      tasks,
+      error,
+      getSecure,
+      getUnsecure,
+      getTasks,
+      getToken,
+      removeToken
+    };
+  }
 });
 
 app.mount("#jwtDemo");
